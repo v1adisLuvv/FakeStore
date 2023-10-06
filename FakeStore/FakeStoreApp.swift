23b046ffc77cdaac479ceece87eb6d1e80df10ce
Vlad <boguzhvlad@gmail.com>
2023-10-06 16:58:36 +0300

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Vlad Boguzh on 06-10-2023.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
